4a66c25e32ef88f358f90ba954fe093c7bfe2823
AnaSousa314 <anasousa314@outlook.com>
2022-06-17 17:51:58 -0300

    feat: create navigation routes

---
+++ src/routes.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import OrphanagesMap from './pages/OrphanagesMap';
+import OrphanageDetails from './pages/OrphanageDetails';
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+
+export default function Routes(){
+  return(
+    <NavigationContainer>
+      <Navigator screenOptions={{headerShown: false}}>
+        <Screen 
+          name="OrphanagesMap"  
+          component={OrphanagesMap}
+          options={{
+            headerShown: false
+          }}
+        />
+        <Screen 
+          name="OrphanageDetails"  
+          component={OrphanageDetails}
+        />
+      </Navigator>
+    </NavigationContainer>
+  )
+}
